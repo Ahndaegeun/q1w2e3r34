66cc69547273bd5d2d44ff115333f822d607cb47
kade <adg0807@naver.com>
2020-12-22 23:40:49 +0900

    js ing

---
+++ scroll.js
@@ -0,0 +1,32 @@
+$(function(){
+    const $header = "header";
+    const $main = ".myWork-section";
+    const $footer = "footer";
+    const $bodyCotainer = ".body__container";
+    const headerHeight = $($header).offset().top;
+    const mainHeight = $($main).offset().top;
+    const footerHeight = $($footer).offset().top;
+
+    $($bodyCotainer).on("mousewheel DOMMouseScroll", function(e){
+
+        let nowScroll = $(document).scrollTop();
+        let delta = e.originalEvent.deltaY;
+
+        if(delta === 100) {
+            //Down
+            if (nowScroll >= 0 && nowScroll < mainHeight) {
+                $('html, body').animate({scrollTop : mainHeight});
+            } else if (nowScroll >= mainHeight && nowScroll < footerHeight) {
+                $('html, body').animate({scrollTop : footerHeight});
+            }
+        } else if (delta === -100) {
+            //Up
+            if (nowScroll <= footerHeight && nowScroll > mainHeight) {
+                $('html, body').animate({scrollTop : mainHeight});
+            } else if (nowScroll <= mainHeight && nowScroll > headerHeight) {
+                $('html, body').animate({scrollTop : headerHeight});
+            }
+        }
+    });
+
+});
